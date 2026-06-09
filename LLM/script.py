03b7147506d2ae0164700706df42272a64fde256
Mouheb Hidouri <hidourimouheb321@gmail.com>
2026-06-09 11:40:20 +0100

    Added loop for LLM requests

--- LLM/script.py
+++ LLM/script.py
@@ -2,7 +2,10 @@ from dotenv import load_dotenv
 import os
 import requests
 key = load_dotenv()
-r = requests.post('https://ollama.com/api/generate', 
-                headers= {"Authorization": "Bearer " + os.environ['OLLAMA_API_KEY']},
-                json= {"model": "gpt-oss:120b", "prompt": "Why is the sky blue?", "stream": False })
-print(r.text)
+while 1:
+    prompt = input("Enter your text : ")
+    r = requests.post('https://ollama.com/api/generate', 
+                    headers= {"Authorization": "Bearer " + os.environ['OLLAMA_API_KEY']},
+                    json= {"model": "gpt-oss:120b", "prompt": prompt, "stream": False })
+    print(r.text)
+    
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # iAnswer
 An AI Customer Support Agent Platform  built from scratch : for companies to upload their docs , and the AI agent answers their question
+--- 
+## Tools Used
+-Ollama Cloud API KEY
+-Python-dotenv to read .env variables
