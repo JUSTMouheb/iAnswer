77551573708e0f40c0f81208d1b88df0fed19016
Mouheb Hidouri <hidourimouheb321@gmail.com>
2026-06-10 23:34:16 +0100

    Added interactive chat loop ( no memory implemented yet)

--- LLM/script.py
+++ LLM/script.py
@@ -2,10 +2,14 @@ from dotenv import load_dotenv
 import os
 import requests
 key = load_dotenv()
-while 1:
+i=0
+while 1 :
+    i+=1
     prompt = input("Enter your text : ")
+    prompts = []
+    prompts.append(prompt)
     r = requests.post('https://ollama.com/api/generate', 
-                    headers= {"Authorization": "Bearer " + os.environ['OLLAMA_API_KEY']},
-                    json= {"model": "gpt-oss:120b", "prompt": prompt, "stream": False })
+                        headers= {"Authorization": "Bearer " + os.environ['OLLAMA_API_KEY']},
+                        json= {"model": "gpt-oss:120b", "prompt": prompts[len(prompts)-1], "stream": False })
     print(r.text)
     
