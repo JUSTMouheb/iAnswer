9ad2b5389fd9d8ee515358e54e4f24412347949b
Mouheb Hidouri <hidourimouheb321@gmail.com>
2026-06-26 15:22:36 +0200

    add retrieval (chunk,embed,cosine rank, top-k context)

--- LLM/script.py
+++ LLM/script.py
@@ -1,20 +1,38 @@
 from dotenv import load_dotenv
 import os
 import requests
+import numpy as np
 f=open("docs/About.txt",'r')
-docs=f.read()
+docs=f.read() #Full Text
+delimiter = "------------------------------------------------------------------------" #what seperates our paragraphs
+chunks = docs.split(delimiter) #chunk = paragraph
+chunks = [chunk.strip() for chunk in chunks]
 key = load_dotenv()
 messages=[]
-messages.append({"role":"system","content":"You are support for Nexalytics. Answer only from these docs: " + docs})
-embeddings = requests.post("http://localhost:11434/api/embed",
-                         json={"model": "mxbai-embed-large", "input": "hello world"})
-print(embeddings)
+length_chunks=len(chunks)
+#Embedding + Tokenizer
+def cosine_sim(A,B):
+    return(np.dot(A,B) / (np.linalg.norm(A) * np.linalg.norm(B)))
+embeddings=[]
+knowledge=[]
+for i in range (length_chunks):
+    vector= requests.post("http://localhost:11434/api/embed",
+                         json={"model": "mxbai-embed-large", "input": chunks[i]}).json()["embeddings"][0]
+    knowledge.append({"text":chunks[i] ,"vector":vector})
 while 1 :
     prompt = input("Enter your text : ")
+    vector_prompt= requests.post("http://localhost:11434/api/embed",
+                         json={"model": "mxbai-embed-large", "input": prompt}).json()["embeddings"][0]    
+    cos_list=[]
+    for i in range(len(knowledge)):
+        cos_list.append({"score":cosine_sim(vector_prompt,knowledge[i]["vector"]),"text":knowledge[i]["text"]})
+    ranked = sorted(cos_list, key=lambda item: item["score"],reverse=True)
+    system_msg = {"role": "system", "content": "You are support for Nexalytics. Answer only from these docs: " + ranked[0]["text"]}    
     messages.append({"role": "user","content": prompt})
     r = requests.post('https://ollama.com/api/chat', 
                         headers= {"Authorization": "Bearer " + os.environ['OLLAMA_API_KEY']},
-                        json= {"model": "gpt-oss:120b", "messages": messages, "stream": False })
+                        json= {"model": "gpt-oss:120b", "messages": [system_msg] + messages, "stream": False })
+    print(messages)
     response_llm=(r.json()["message"]["content"])
     messages.append({"role":"assistant","content": response_llm})
-    print(response_llm)
+    print(response_llm) 
