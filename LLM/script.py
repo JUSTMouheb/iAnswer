0017439d8cd8eecdeaa15c7b4164e81ec0d3aa50
Mouheb Hidouri <hidourimouheb321@gmail.com>
2026-06-29 22:39:11 +0200

    docs : add explanation for function

--- LLM/script.py
+++ LLM/script.py
@@ -15,13 +15,21 @@ def cosine_sim(A,B):
     return(np.dot(A,B) / (np.linalg.norm(A) * np.linalg.norm(B)))
 knowledge=[]
 def embed(text):
+    '''
+    Embed function : Embedding for text chunks extracted from the paragraphs we seperated.
+    '''
     return(requests.post("http://localhost:11434/api/embed",
                          json={"model": "mxbai-embed-large", "input": text}).json()["embeddings"][0])
 for i in range (length_chunks):
     vector = embed(chunks[i])
     knowledge.append({"text":chunks[i] ,"vector":vector})
-
 def retrieve(prompt):
+    '''Retrieve Function : Contains 4 steps:
+    1/Embed the prompt the user typed
+    2/Create Cos_list
+    3/Distinguish for each paragraph(a.k.a text) the cosine_sim
+    4/sort the cosine_sim
+    5/return the best'''
     vector_prompt = embed(prompt)
     cos_list = []
     for i in range(len(knowledge)):
